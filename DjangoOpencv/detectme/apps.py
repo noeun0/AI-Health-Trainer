57cd0597216545dc56c25df5d2ef442a38cba3d6
noeun0 <61595578+noeun0@users.noreply.github.com>
2021-06-22 10:47:10 +0900

    django

--- DjangoOpencv/detectme/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DetectmeConfig(AppConfig):
-    name = 'detectme'
--- DjangoOpencv/detectme/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('detectme', views.detectme, name="detectme"),
-    ]
